e60b33da2f45edd302c13f092f8b1b8508e279a9
Matt Brictson <matt@mattbrictson.com>
2024-06-07 14:22:57 -0700

    Clean up Ubuntu setup script

--- .docker/ubuntu_setup.sh
+++ .docker/ubuntu_setup.sh
@@ -11,15 +11,13 @@ cp /root/.ssh/authorized_keys /home/deployer/.ssh
 chown -R deployer:deployer /home/deployer/.ssh
 chmod 600 /home/deployer/.ssh/authorized_keys
 
-# TODO: This was to avoid errors during update that looked like
-# GPG error: http://security.ubuntu.com/ubuntu jammy-security InRelease: At least one invalid signature was encountered.
-
-apt -y update --allow-insecure-repositories
-
 # Install and configure sshd
+apt -y update
 apt-get -y install openssh-server git
-echo "Port 22" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
+{
+  echo "Port 22"
+  echo "PasswordAuthentication no"
+  echo "ChallengeResponseAuthentication no"
+} >> /etc/ssh/sshd_config
 mkdir /var/run/sshd
 chmod 0755 /var/run/sshd
